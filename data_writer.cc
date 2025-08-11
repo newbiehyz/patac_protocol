cd70c506cf64438b2dc5eb51ff56b042c9990baf
jianing_huang <jianing_huang@apa.patac>
2025-08-11 10:29:10 +0800

    sync imgs write

--- data_writer.cc
+++ data_writer.cc
@@ -237,6 +237,37 @@ void DataWriter::WriteSlotMsg(const long long timestamp,
   sqlite3_finalize(stmt_seq);
 }
 
+void DataWriter::WriteSlotMsg(const long long timestamp,
+                              const class patac_hpp::ParkingSlotList &slot_uv) {
+  std::string insert_data_slot =
+      "INSERT INTO slot_list (timestamp, data) VALUES (?, ?);";
 
+  patac_hpp::ParkingSlotList ps_list;
+  ps_list = slot_uv;
+  std::string serialized;
+  ps_list.SerializeToString(&serialized);
+  sqlite3_stmt *stmt_seq = nullptr;
+  sqlite3_prepare_v2(_db, insert_data_slot.c_str(), -1, &stmt_seq, nullptr);
+
+  sqlite3_bind_int64(stmt_seq, 1, timestamp);
+  sqlite3_bind_blob(stmt_seq, 2, serialized.data(), serialized.size(),
+                    SQLITE_TRANSIENT);
+
+  int rc = sqlite3_step(stmt_seq);
+  if (rc != SQLITE_DONE) {
+    std::cerr << "Insert into slot_list failed: " << sqlite3_errmsg(_db)
+              << std::endl;
+  } else {
+    // std::cout << "Successfully inserted pose at " << timestamp << std::endl;
+  }
+
+  // if (sqlite3_step(stmt_seq) != SQLITE_DONE) {
+  //   std::cerr << "Insert into dr_pose failed: " << sqlite3_errmsg(_db)
+  //             << std::endl;
+  // }
+
+  sqlite3_reset(stmt_seq);
+  sqlite3_finalize(stmt_seq);
+}
 
 }  // namespace apa_slam
